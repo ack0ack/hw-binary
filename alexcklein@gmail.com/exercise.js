16b83c086abfc247edfe8f0cdab25a7114834d25
alexcklein@gmail.com <alexcklein@gmail.com>
2018-02-22 16:53:18 -0500

    Updated exercise file(s)

--- alexcklein@gmail.com/exercise.js
+++ alexcklein@gmail.com/exercise.js
@@ -2,28 +2,6 @@ var exercise = {};
 
 
 exercise.roman = function(time){
-
-    //-------------------------------------
-    //  YOUR CODE
-    //    Return an object with roman time. 
-    //    Time is reported using 6 columns.
-    //
-    //  For example, for 05:13:47 PM
-    //    { 
-    //       hour_col1 : 1, 
-    //       hour_col2 : 7,
-    //       min_col1  : 1, 
-    //       min_col2  : 3, 
-    //       sec_col1  : 4, 
-    //       sec_col2  : 7
-    //     };
-    // 
-    //-------------------------------------
-
-};
-
-exercise.binary = function(time){
-
     var hours = time.getHours();
     var minutes = time.getMinutes(); 
     var seconds = time.getSeconds();
@@ -33,7 +11,6 @@ exercise.binary = function(time){
         minutes = "0" + minutes};
     if(seconds<10){
         seconds = "0" + seconds};
-    console.log(seconds);
 
     var secondsArray=[];
     var secondsString = seconds.toString();
@@ -53,132 +30,89 @@ exercise.binary = function(time){
     var column4 = minutesArray[1];
     var column5 = minutesArray[0];
     var column6 = minutesArray[1];
+
+    console.log(column1);
+
+    var roman={
+        hour_co11 : column1,
+        hour_co12 : column2,
+        min_co11 : column3,
+        min_co12 : column4,
+        sec_co11 : column5,
+        sec_co12 : column6,
+    }
+
+    console.log(roman);
+    return roman;
+
+} 
+
+    //-------------------------------------
+    //  YOUR CODE
+    //    Return an object with roman time. 
+    //    Time is reported using 6 columns.
+    //
+    //  For example, for 05:13:47 PM
+    //    { 
+    //       hour_col1 : 1, 
+    //       hour_col2 : 7,
+    //       min_col1  : 1, 
+    //       min_col2  : 3, 
+    //       sec_col1  : 4, 
+    //       sec_col2  : 7
+    //     };
+    // 
+    //-------------------------------------
+
+
+exercise.binary = function(time){
+
+    var hours = time.getHours();
+    var minutes = time.getMinutes(); 
+    var seconds = time.getSeconds();
+    if(hours<10){
+        hours = "0" + hours};
+    if(minutes<10){
+        minutes = "0" + minutes};
+    if(seconds<10){
+        seconds = "0" + seconds};
+
+    var secondsArray=[];
+    var secondsString = seconds.toString();
+    secondsArray = secondsString.split("")
+
+    var minutesArray=[];
+    var minutesString = minutes.toString();
+    minutesArray = minutesString.split("")
+
+    var hoursArray=[];
+    var hoursString = hours.toString();
+    hoursArray = hoursString.split("");
+
+    var column1 = hoursArray[0];
+    var column2 = hoursArray[1];
+    var column3 = minutesArray[0];
+    var column4 = minutesArray[1];
+    var column5 = minutesArray[0];
+    var column6 = minutesArray[1];
     
     var binary = {
-        if(column1===0){
-            col1.position8= 'off';
-            col1.position4= 'off';
-            col1.position2= 'off';
-            col1.position1= 'off';
-        }
-        if(col1===1){
-            col1.position8= 'off';
-            col1.position4= 'off';
-            col1.position2= 'off';
-            col1.position1= 'on';
-        }
-        if(col1===2){
-            col1.position8= 'off';
-            col1.position4= 'off';
-            col1.position2= 'on';
-            col1.position1= 'off';
-        }
-        if(col1===3){
-            col1.position8= 'off';
-            col1.position4= 'off';
-            col1.position2= 'on';
-            col1.position1= 'on';
-        }
-        if(col1===4){
-            col1.position8= 'off';
-            col1.position4= 'on';
-            col1.position2= 'off';
-            col1.position1= 'off';
-        }
-        if(timeNumbers[0]===5) {
-            col1.position8= 'off';
-            col1.position4= 'on';
-            col1.position2= 'off';
-            col1.position1= 'on';
-        }
-        if(timeNumbers[0]===6) {
-            col1.position8= 'off';
-            col1.position4= 'on';
-            col1.position2= 'on';
-            col1.position1= 'off';
-        }
-        if(timeNumbers[0]===7) {
-            col1.position8= 'off';
-            col1.position4= 'on';
-            col1.position2= 'on';
-            col1.position1= 'on';
-        }
-        if(timeNumbers[0]===8) {
-            col1.position8= 'on';
-            col1.position4= 'off';
-            col1.position2= 'off';
-            col1.position1= 'off';
-        }
-        if(timeNumbers[0]===9) {
-            col1.position8= 'on';
-            col1.position4= 'off';
-            col1.position2= 'off';
-            col1.position1= 'on';
+        column1:{},
+        column2:{},
+        column3:{},
+        column4:{},
+        column5:{},
+        column6:{},
+    };
 
-
-            if(timeNumbers[1]===0) {
-                col1.position8= 'off';
-                col1.position4= 'off';
-                col1.position2= 'off';
-                col1.position1= 'off';
-            }
-            if(timeNumbers[1]===1) {
-                col1.position8= 'off';
-                col1.position4= 'off';
-                col1.position2= 'off';
-                col1.position1= 'on';
-            }
-            if(timeNumbers[1]===2) {
-                col1.position8= 'off';
-                col1.position4= 'off';
-                col1.position2= 'on';
-                col1.position1= 'off';
-            }
-            if(timeNumbers[1]===3) {
-                col1.position8= 'off';
-                col1.position4= 'off';
-                col1.position2= 'on';
-                col1.position1= 'on';
-            }
-            if(timeNumbers[1]===4) {
-                col1.position8= 'off';
-                col1.position4= 'on';
-                col1.position2= 'off';
-                col1.position1= 'off';
-            }
-            if(timeNumbers[1]===5) {
-                col1.position8= 'off';
-                col1.position4= 'on';
-                col1.position2= 'off';
-                col1.position1= 'on';
-            }
-            if(timeNumbers[1]===6) {
-                col1.position8= 'off';
-                col1.position4= 'on';
-                col1.position2= 'on';
-                col1.position1= 'off';
-            }
-            if(timeNumbers[1]===7) {
-                col1.position8= 'off';
-                col1.position4= 'on';
-                col1.position2= 'on';
-                col1.position1= 'on';
-            }
-            if(timeNumbers[1]===8) {
-                col1.position8= 'on';
-                col1.position4= 'off';
-                col1.position2= 'off';
-                col1.position1= 'off';
-            }
-            if(timeNumbers[1]===9) {
-                col1.position8= 'on';
-                col1.position4= 'off';
-                col1.position2= 'off';
-                col1.position1= 'on';
-    }
-
+    if(column1 == '1'){
+        binary.column1.position8= 'on';
+        binary.column1.position4= 'on';
+        binary.column1.position2= 'off';
+        binary.column1.position1= 'on';
+    } 
     return binary;
-    }
+    };
     
 
     //----------------------------------------------------------
@@ -197,4 +131,3 @@ exercise.binary = function(time){
     // 
     //----------------------------------------------------------
 
-};
